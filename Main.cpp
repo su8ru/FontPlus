5a1ef38b38fdaab11da02317fcb894147eccffdb
subaru2003 <subaru2003.main@gmail.com>
2019-05-30 16:19:21 +0900

    Add Files.

---
+++ Main.cpp
@@ -0,0 +1,15 @@
+﻿# include <Siv3D.hpp> // Siv3D August 2016 v2
+# include "./FontPlus/FontP.hpp"
+
+void Main()
+{
+	const FontP font(30);
+	FontAsset::Register(L"font", 30, L"Yu Gothic UI Regular");
+
+	while (System::Update())
+	{
+		((DrawableStringP)FontAsset(L"font")(L"＊This is old Siv3D.")).draw(Arg::bottomRight, Window::Size());
+
+		font(L"＊This is old Siv3D.").draw(Arg::center, Window::Center());
+	}
+}
